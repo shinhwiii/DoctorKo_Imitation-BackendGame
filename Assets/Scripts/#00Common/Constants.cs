e3b49ea3307d0842487e59c2b9376161d9949535
shinhwiii <sh0122451@gmail.com>
2024-02-13 17:52:15 +0900

    feat: "랭킹" 기능을 이용한 유저 랭킹 관리 (랭킹 출력)

--- Assets/Scripts/#00Common/Constants.cs
+++ Assets/Scripts/#00Common/Constants.cs
@@ -2,4 +2,5 @@ public class Constants
 {
     public static readonly string USER_DATA_TABLE = "USER_DATA";
     public static readonly string DAILY_RANK_UUID = "27f58c40-ca42-11ee-a57d-7913a95ac03f";
+    public static readonly int MAX_RANK_LIST = 20;
 }
